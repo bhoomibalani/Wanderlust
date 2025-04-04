d031c1881e4fd7c7b509a071a196987585f92b0c
bhoomibalani <balanibhoomi8@gmail.com>
2025-04-05 02:16:43 +0530

    Implemented authentication using Passport middleware

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ const ejsMate = require("ejs-mate");
 const session = require("express-session");
 const ExpressError = require("./utils/ExpressError.js");
 const flash = require("connect-flash");
+const passport=require("passport");
+const LocalStrategy=require("passport-local");
+const User=require("./models/user.js");
 
-
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter=require("./routes/user.js");
 //database
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
 
@@ -50,11 +53,29 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
-})
+});
+
+// app.get("/demouser",async(req,res)=>{
+//     let fakeUser=new User({
+//         email:"student@s.ocm",
+//         username:"delta-student"
+//     });
+
+//     let registeredUser=await User.register(fakeUser,"helloworld");
+//     res.send(registeredUser);
+// });
+
 const validateReview = (req, res, next) => {
     let { error } = reviewSchema.validate(req.body);
 
@@ -66,8 +87,9 @@ const validateReview = (req, res, next) => {
     }
 };
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/",userRouter);
 
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
